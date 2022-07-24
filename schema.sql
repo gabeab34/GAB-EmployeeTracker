1e2ba147b9e3f01791780511e7067691033fa660
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-24 17:27:20 -0400

    changed table values to matach assignment image

--- schema.sql
+++ schema.sql
@@ -5,20 +5,20 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_title VARCHAR(30),
-    role_salary DECIMAL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
