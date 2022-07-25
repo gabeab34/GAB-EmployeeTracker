fd6c7636ca45075b455cc505dcac22914b71c8fa
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-25 17:17:16 -0400

    added more specific questions to application

--- server.js
+++ server.js
@@ -16,6 +16,35 @@ StartQs = () => {
     name: 'initialQ',
     type: 'list',
     message: 'Welcome to the employee database. Please select the action you would like to perform.',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role' ]
-    },   
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role' ]
+    },
+    {
+    name: 'departmentAdd',
+    type: 'input',
+    message: 'Please enter a name for the new department',
+    when: ({ initialQ }) => initialQ === 'Add a department'
+    },
+    {
+    name: 'roleAdd',
+    type: 'input',
+    message: 'Please enter a name for the new role',
+    when: ({ initialQ }) => initialQ === 'Add a role'
+    },
+    {
+    name: 'employeeAddFirst',
+    type: 'input',
+    message: 'Please enter a first name for the new employee',
+    when: ({ initialQ }) => initialQ === 'Add an employee'
+    },
+    {
+    name: 'employeeAddLast',
+    type: 'input',
+    message: 'Please enter a last name for the new employee',
+    when: ({ initialQ }) => initialQ === 'Add an employee'
+    },
+
+
+
+
+
 ])}
