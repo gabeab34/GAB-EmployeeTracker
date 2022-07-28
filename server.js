72557151075e47e8b174afe2acd84a93722c3303
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-27 21:53:02 -0400

    removed extraneous code that was no longer needed

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import dbconnect from './config/connection.js';
 import inquirer from 'inquirer';
 
-// import table from 'console.table'
-// const cTable = require('console.table');
-
 
 const Qs = async () => {
    await inquirer.prompt([
@@ -12,20 +9,6 @@ const Qs = async () => {
     type: 'list',
     message: 'Welcome to the employee database. Please select the action you would like to perform.',
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role' ]
-    },
-
-
-    {
-    name: 'employeeAddFirst',
-    type: 'input',
-    message: 'Please enter a first name for the new employee',
-    when: ({ initialQ }) => initialQ === 'Add an employee'
-    },
-    {
-    name: 'employeeAddLast',
-    type: 'input',
-    message: 'Please enter a last name for the new employee',
-    when: ({ initialQ }) => initialQ === 'Add an employee'
     },
     ])
     .then((userInput) => {
@@ -123,7 +106,7 @@ const addRole = () => {
 const addEmployee = () => {
     dbconnect.query(`SELECT * From role;`, (err,res) => {
         if (err) throw err;
-        let roles = res.map(role => ({name: role.title, value: role.role_id}));
+        let roles = res.map(role => ({name: role.title, value: role.id}));
         dbconnect.query(`SELECT * FROM employee`, (err, res) => {
             if (err) throw err;
             let employees = res.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}))
@@ -165,7 +148,7 @@ const addEmployee = () => {
 const updateEmployee = () => {
     dbconnect.query(`SELECT * FROM role;`, (err, res) => {
         if (err) throw err;
-        let roles = res.map(role => ({name: role.title, value: role.role_id}));
+        let roles = res.map(role => ({name: role.title, value: role.id}));
         dbconnect.query(`SELECT * FROM employee;`, (err, res) => {
             if (err) throw err;
             let employees = res.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}));
@@ -192,26 +175,6 @@ const updateEmployee = () => {
      )}
   )}
 )};
-
-
-
-
-
-
-  
-// const viewDepartment = async () => {
-//     let data = await dbconnect.promise().query('SELECT * from department');
-//     console.table(data[0])
-// }
-
-// const viewDepartments = async () =>
-//     await dbconnect.query(`SELECT * FROM department ORDER BY id ASC;`, function (err, res) {
-//         if (err) throw err;
-//         console.table(res);
-//     });
-
-
-
 
 
 Qs();
